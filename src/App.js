dcba4ba70fd5893139498f483a12b640fd70c440
coder439 <65747567+coder439@users.noreply.github.com>
2021-11-14 18:34:50 -0600

    components added and started styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './components/Header'
+import BodyContent from './components/BodyContent'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
       <Header/>
+      <BodyContent/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+    return <div className="footerComponent">
+        <div> Explorer - National Parks allows you to explore activities, which National Parks they are available at, and learn more about National Parks that interest you! </div>
+        <div> Made by Pavan Alluri. Powered by the National Park Service API and React.js. Hosted on Github Pages.</div>
+
+    </div>
+
+}
+
+
+export default Footer
